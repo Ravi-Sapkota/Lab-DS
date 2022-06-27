aa0e01c6917cd4c7353e7c5f4a610d865ce4d895
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-27 23:07:31 +0545

    Update Lab 11.c

--- Lab 11.c
+++ Lab 11.c
@@ -1,1 +1,12 @@
 // Write a C program to print truth table of AND Operation
+
+#include <stdio.h>
+
+int main() {
+  int a,b;
+  printf("A\tB\tA&B\n");
+  for(a=0;a<=1;a++)
+    for(b=0;b<=1;b++)
+      printf("%d\t%d\t %d\n",a,b,a&&b);
+  return 0;
+}
