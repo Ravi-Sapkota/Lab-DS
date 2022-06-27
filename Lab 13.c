e5f1d8c834e27186b2153bddff511734f07cedcb
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-27 23:17:42 +0545

    Update Lab 13.c

--- Lab 13.c
+++ Lab 13.c
@@ -1,1 +1,11 @@
 // Write a C program to print truth table of NOT Operation
+
+#include <stdio.h>
+
+int main() {
+  int a;
+  printf("A\tA'\n");
+  for(a=0;a<=1;a++)
+      printf("%d\t%d\n",a,!a);
+  return 0;
+}
