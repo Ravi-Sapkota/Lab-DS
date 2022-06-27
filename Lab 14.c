1f04549ccefbe2b7ca33e9e17cbe9fad58512f2c
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-27 20:52:14 +0545

    Update Lab 14.c

--- Lab 14.c
+++ Lab 14.c
@@ -1,1 +1,19 @@
 // Write a C program to print truth table of IMPLICATION Operation
+
+#include <stdio.h>
+
+int implication(int x, int y) {
+  if(x==1 && y==0){
+      return 0;
+  }else{
+      return 1;
+  }
+}
+int main() {
+  int a,b;
+  printf("A\tB\tA->B\n");
+  for(a=0;a<=1;a++)
+    for(b=0;b<=1;b++)
+      printf("%d\t%d\t %d\n",a,b,implication(a,b));
+  return 0;
+}
