288e4c661f9fe681962be3a8df55c636d99237f6
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-27 20:34:23 +0545

    Update _Lab 4_1.c

--- _Lab 4_1.c
+++ _Lab 4_1.c
@@ -1,17 +1,17 @@
-// WAP to compute the factorial of a number using a recursive function.
+// Write a C program to find validity of arguments by using truth table
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int fact(int x){
-  if(x>1){
-    return x*fact(x-1);
-  }else{
-    return 1;
-  }
-}
+// int fact(int x){
+//   if(x>1){
+//     return x*fact(x-1);
+//   }else{
+//     return 1;
+//   }
+// }
 
-int main(){
-  int x=5;
-  printf("The factorial of %d is %d",x,fact(x));
-  return 0;
-} 
+// int main(){
+//   int x=5;
+//   printf("The factorial of %d is %d",x,fact(x));
+//   return 0;
+// } 
