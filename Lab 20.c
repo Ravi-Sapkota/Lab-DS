d81875f6a11aa9b2e3987248e0edf5da08a5274c
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-27 23:06:22 +0545

    Update Lab 20.c

--- Lab 20.c
+++ Lab 20.c
@@ -1,33 +1,38 @@
 // Write a program to implement linear search using recursion
 
 #include <stdio.h>
-
-int LinearSearch(int arr[],int s,int r,int x){
-    if(r<x)
-        return -1;
-    if(arr[s]==x)
-        return s;
-    return LinearSearch(arr,s+1,r,x);
-}
-
-int main(){
-    int n, key,pos;
-    printf("Enter the size of array.\t");
-    scanf("%d",&n);
-    int array[n];
-    printf("Enter the elements of the array\n");
-    for(int i=0;i<n;i++){
-        scanf("%d",&array[i]);
-    }
-    printf("Enter the element to be searched\n");
-    scanf("%d",&key);
-    
-    pos=LinearSearch(array,0,n-1,key);
-    
-    if(pos==-1){
-        printf("Element %d is not present in the given array",key);
+int RecursiveLS(int arr[], int value, int index, int n){
+    int pos = 0;
+    if(index >= n){
+        return 0;
+    }else if (arr[index] == value){
+        pos = index + 1;
+        return pos;
     }else{
-        printf("Element %d is present at position %d",key,pos+1);
+        return RecursiveLS(arr, value, index+1, n);
+    }
+    return pos;
+}
+ 
+int main(){
+    int n, value, pos, m = 0, arr[100];
+    printf("Enter the total elements in the array  ");
+    scanf("%d", &n);
+ 
+    printf("Enter the array elements\n");
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+ 
+    printf("Enter the element to search  ");
+    scanf("%d", &value);
+ 
+    pos =  RecursiveLS(arr, value, 0, n);
+    if (pos != 0){
+        printf("Element found at pos %d ", pos);
+    }
+    else{
+        printf("Element not found");
     }
     return 0;
 }
