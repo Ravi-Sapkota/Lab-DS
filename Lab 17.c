62de231c6d4baea461f6ea38afcb7e5f1f46fb26
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-28 00:05:15 +0545

    Update Lab 17.c

--- Lab 17.c
+++ Lab 17.c
@@ -1,17 +1,2 @@
 // Write a C program to print truth table for tautology and contradiction operations
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    int p,q;
-    puts("p\tq\tp^q\tq^p");
-    for(p=0;p<2;p++)
-        for(q=0;q<2;q++){
-            printf("%d\t%d\t %d \t %d\n",p,q,p&&q,q&&p);
-            if(p&&q != q&&p) puts("Not Valid"), exit(1);
-        }
-    puts("Hence, the statement is valid");
-    
-    return 0;
-}
