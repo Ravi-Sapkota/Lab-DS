7df3b1ffc4b412e8f51a83b175c91940548936b6
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-28 02:30:21 +0545

    Update Lab 10.c

--- Lab 10.c
+++ Lab 10.c
@@ -2,42 +2,28 @@
 
 #include <stdio.h>
 
-#define N 3
-
-int A[N][N];
-int B[N][N];
-int i, j;
+#define N 2
 
 void operation(int A[N][N], int B[N][N]){
+    int i,j;
     puts("Join matrix:");
     for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
+        for(j=0;j<N;j++)
             printf("%d ",A[i][j] | B[i][j]);
-        }
         puts("");
     }
-    puts("Product matrix:");
+    
+    puts("\nProduct matrix:");
     for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
+        for(j=0;j<N;j++)
             printf("%d ",A[i][j] & B[i][j]);
-        }
         puts("");
     }
 }
 
 int main(){
-  puts("Enter the first boolean matrix row-wise here:");
-  for(i=0;i<N;i++){
-      for(j=0;j<N;j++){
-          scanf("%d",&A[i][j]);
-      }
-  }
-  puts("Enter the second boolean matrix row-wise here:");
-  for(i=0;i<N;i++){
-      for(j=0;j<N;j++){
-          scanf("%d",&B[i][j]);
-      }
-  }
+  int A[N][N] = {{1,0},{0,1}};
+  int B[N][N] = {{1,1},{0,0}};
   operation(A,B);
   return 0;
 }  
