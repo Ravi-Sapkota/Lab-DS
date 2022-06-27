6f96c1aee432daabde185a4f5a2b392f4fd0ee23
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-27 20:32:47 +0545

    Update and rename _Lab 4_4.c to Lab 19.c

---
+++ Lab 19.c
@@ -0,0 +1,18 @@
+// Write a program to compute a^n mod m  using recursion
+
+//#include<stdio.h>
+
+//int product(int a, int b){
+//  if(a>1){
+//    return b+product(--a,b);
+//  }else{
+//    return b;
+//  }
+//}
+
+//int main(){
+//  int first_num=5;
+//  int second_num=6;
+//  printf("%d*%d=%d",first_num,second_num,product(first_num,second_num));
+//  return 0;
+//}
