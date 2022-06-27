8f83abb03ee9387e90b5d109882e71d191f6fef9
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-27 20:33:45 +0545

    Update and rename _Lab 4_2.c to Lab 17.c

---
+++ Lab 17.c
@@ -0,0 +1,23 @@
+// Write a C program to print truth table for tautology and contradiction operations
+
+//#include<stdio.h>
+
+//int fibonacchi(int first,int second,int position){
+//  static int third;
+//  third = first+second;
+//  first=second;
+//  second=third;
+//  --position;
+//  if(position==0){
+//    return third;
+//  }else{
+//      return fibonacchi(first, second, position);
+//  }
+//}
+
+//int main(){
+//  // series is 0 1 1 2 3 5 8 13 21 34 55 
+//  int position = 10;
+//  printf("The %dth fibonacchi number is %d.",position, fibonacchi(0, 1, position-2));
+//  return 0;
+//}
