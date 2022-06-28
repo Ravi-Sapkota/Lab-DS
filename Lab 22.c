16dc9fcf84626bf20b8297a1ac6c4864e99187ac
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-28 08:32:55 +0545

    Update Lab 22.c

--- Lab 22.c
+++ Lab 22.c
@@ -2,27 +2,14 @@
 
 #include <stdio.h>
 
-int permutation(int n, int r){
-    int i;
-    long p=1,q=1;
-    for(i=1;i<=n;i++){
-        p *=i;
-    }
-    for(i=1;i<=(n-r);i++){
-        q *= i;
-    }
-    return p/q;
+int fact(int x){
+    if(x==0) return 1;
+    return x*fact(x-1);
 }
 
-int combination(int n, int r){
-    int i;
-    long s=1;
-    int x= permutation(n,r);
-    for(i=1;i<=r;i++){
-        s *= i;
-    }
-    return x/s;
-}
+int permutation(int n, int r){ return fact(n)/fact(n-r); }
+
+int combination(int n, int r){ return fact(n)/(fact(n-r)*fact(r)); }
 
 int main(){
     int n, r, p, c;
