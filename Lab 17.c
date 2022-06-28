981432b2e03596afdaf4c69d8f1aecd4005ed549
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-28 07:42:01 +0545

    Update Lab 17.c

--- Lab 17.c
+++ Lab 17.c
@@ -1,2 +1,18 @@
 // Write a C program to print truth table for tautology and contradiction operations
 
+#include<stdio.h>
+
+int main(){
+    int p;
+    printf("p\t-p\tpv-p\n");
+    for(p=0;p<=1;p++){
+        printf("%d\t%d\t %d\n",p,!p,p || !p);
+    }
+    puts("This is tautology");
+    printf("p\t-p\tp^-p\n");
+    for(p=0;p<=1;p++){
+        printf("%d\t%d\t %d\n",p,!p,p && !p);
+    }
+    puts("This is contradiction");
+    return 0;
+}
