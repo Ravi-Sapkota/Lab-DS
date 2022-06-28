8c3b459326cad348f3e2ccaca8b85d464bf826b1
White Shadow <82247249+Ravi-Sapkota@users.noreply.github.com>
2022-06-28 08:24:25 +0545

    Update Lab 21.c

--- Lab 21.c
+++ Lab 21.c
@@ -1,22 +1,16 @@
 // Write a program to implement binary search using recursion
 
+
 #include <stdio.h>
 
 int binary_search(int [], int, int, int);
-void bubble_sort(int [], int);
+// void bubble_sort(int [], int);
 
 int main(){
-    int n, key,pos;
-    printf("Enter the size of array.\t");
-    scanf("%d",&n);
-    int array[n];
-    printf("Enter the %d elements of the array\n",n);
-
-    for(int i=0;i<n;i++){
-        scanf("%d",&array[i]);
-    }
-
-    bubble_sort(array, n);
+    int n=10, key,pos;
+    int array[]={0,1,2,3,4,5,6,7,8,9};
+    
+    // bubble_sort(array, n);
     printf("Enter the element to be searched\n");
     scanf("%d",&key);
     
@@ -30,28 +24,27 @@ int main(){
     return 0;
 }
 
-void bubble_sort(int list[], int s){
-    int temp, i, j;
-    for (i = 0; i < s; i++){
-        for (j = i; j < s; j++){
-            if (list[i] > list[j]){
-                temp = list[i];
-                list[i] = list[j];
-                list[j] = temp;
-            }
-        }
-    }
-    printf("\n\nThe sorted array is:\n");
-    for(i=0;i<s;i++){
-        printf("%d ",list[i]);
-    }
-    printf("\n\n");
-}
+// void bubble_sort(int list[], int s){
+//     int temp, i, j;
+//     for (i = 0; i < s; i++){
+//         for (j = i; j < s; j++){
+//             if (list[i] > list[j]){
+//                 temp = list[i];
+//                 list[i] = list[j];
+//                 list[j] = temp;
+//             }
+//         }
+//     }
+//     printf("\n\nThe sorted array is:\n");
+//     for(i=0;i<s;i++){
+//         printf("%d ",list[i]);
+//     }
+//     printf("\n\n");
+// }
 
 int binary_search(int list[], int l, int h, int key){
     int mid;
     if (l > h){
-        printf("Key not found.\n");
         return -1;
     }
     mid = (l + h) / 2;
